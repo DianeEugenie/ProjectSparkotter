cda528059653c78b5e8dade9e6005f472f3728d6
Gillian Shanks <GillianShanks88@gmail.com>
2019-10-18 16:01:26 +0100

    Adds bootstrapped java spring backend folder

---
+++ sparkotter_backend/src/main/java/com/example/codeclan/sparkotter_backend/SparkotterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.sparkotter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkotterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkotterBackendApplication.class, args);
+	}
+
+}
