7fd7298b5842c074d60105a5f5aacc84da54703b
Gillian Shanks <GillianShanks88@gmail.com>
2019-10-20 18:58:17 +0100

    Controllers made for Prompt and CreativeInstance - loads in browser

--- sparkotter_backend/src/main/java/com/example/codeclan/sparkotter_backend/controllers/InstanceController.java
+++ sparkotter_backend/src/main/java/com/example/codeclan/sparkotter_backend/controllers/InstanceController.java
@@ -1,14 +1,14 @@
 package com.example.codeclan.sparkotter_backend.controllers;
 
-import com.example.codeclan.sparkotter_backend.repositories.PromptRepository.PromptRepository;
+import com.example.codeclan.sparkotter_backend.repositories.InstanceRepository.InstanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prompts")
-public class PromptController {
+@RequestMapping("/creative_instances")
+public class InstanceController {
 
     @Autowired
-    PromptRepository promptRepository;
+    InstanceRepository instanceRepository;
 }
