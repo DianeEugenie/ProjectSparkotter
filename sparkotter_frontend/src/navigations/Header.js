90fa17a82c01945b85a96912f0b0394d04b5162a
Martin Porter <martinporter1312@gmail.com>
2019-10-21 11:08:02 +0100

    adds helper file

--- sparkotter_frontend/src/navigations/Header.js
+++ sparkotter_frontend/src/navigations/Header.js
@@ -1,1 +1,18 @@
 //<Link to="/options"></Link>
+import React,{Fragment} from 'react';
+
+
+
+const Header = (props) => {
+  return (
+
+    <Fragment>
+      <p>Image goes here</p>
+
+
+    </Fragment>
+
+
+  )
+}
+export default Header;
