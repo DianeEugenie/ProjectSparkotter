1ffc0cc5eb592d7df4d59a8a21587dc83143989e
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-10-22 14:57:08 +0100

    Adds target to twitter so it opens in new page

--- sparkotter_frontend/src/components/time_elements/TimesUpPage.js
+++ sparkotter_frontend/src/components/time_elements/TimesUpPage.js
@@ -21,7 +21,7 @@ const TimesUpPage = (props) => {
     <img className="times-up-logo" src="https://media.discordapp.net/attachments/318372572775317505/635594327724392458/timesup.png" />
     <Link to={optionsUrl}><button className="start-button" onClick={handleTimesUp}>New Spark</button></Link>
     <a href={sparkiveUrl}><button className="start-button" onClick={handleTimesUp}>Your Sparkive</button></a>
-    <a className="twitter-share-button" href={tweetText}>
+    <a className="twitter-share-button" href={tweetText} target="_blank">
     Tweet</a>
 
     </Fragment>
