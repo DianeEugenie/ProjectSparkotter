44a9470a1577f6940d2216af88af6ad8b400e54e
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-10-22 18:53:51 +0100

    Updates twitter link

--- sparkotter_frontend/src/components/time_elements/TimesUpPage.js
+++ sparkotter_frontend/src/components/time_elements/TimesUpPage.js
@@ -21,7 +21,7 @@ const TimesUpPage = (props) => {
     <img alt="Sparky celebrating you are done" className="times-up-logo" src="https://media.discordapp.net/attachments/318372572775317505/635594327724392458/timesup.png" />
     <Link to={optionsUrl}><button className="start-button" onClick={handleTimesUp}>New Spark</button></Link>
     <a href={sparkiveUrl}><button className="start-button" onClick={handleTimesUp}>Your Sparkive</button></a>
-    <a className="twitter-share-button" href={tweetText} target="_blank">
+    <a className="twitter-share-button" href={tweetText} target="_blank" rel="noopener noreferrer">
     Tweet</a>
 
     </Fragment>
