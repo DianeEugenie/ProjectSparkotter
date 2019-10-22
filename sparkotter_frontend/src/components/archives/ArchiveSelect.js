71b838a18ef07d2b2f33ec7a16dd5e237b5413e7
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-10-22 15:12:05 +0100

    Imports moment for date format

--- sparkotter_frontend/src/components/archives/ArchiveSelect.js
+++ sparkotter_frontend/src/components/archives/ArchiveSelect.js
@@ -1,25 +1,23 @@
 import React, {Component} from 'react';
-
-
+import moment from 'moment';
 
 const ArchiveSelect = (props) => {
 
+  const handleSelect = () => {
+
+  }
+
+  const archiveOptions = props.selections.map((archiveItem, index ) => {
+    return <option key={index} value={archiveItem}>{moment(archiveItem.dateCreated).local().format('LL')}</option>
+  })
+
+  return (<div>
+    <select name="archiveItem" onChange={handleSelect}>
+    {archiveOptions}
+    </select>
+    </div>)
+
+}
 
 
-  // render(){
-    const archiveOptions = props.selections.map((archiveItem, index ) => {
-      return <option key={index} value={archiveItem}>{archiveItem.dateCreated}</option>
-    })
-
-    return (<div>
-      <select name="archiveItem">
-      {archiveOptions}
-      </select>
-      </div>)
-    }
-
-
-
-
-
-  export default ArchiveSelect;
+export default ArchiveSelect;
